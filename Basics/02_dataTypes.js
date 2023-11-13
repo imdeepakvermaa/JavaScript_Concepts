6cec958dd46eaa4d0fa5134b1ccb69bb6703fa15
imdeepakvermaa <dv24012002@gmail.com>
2023-11-13 13:13:37 +0530

    datatyoes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use Strict"; // treat all JS code as newer version
+
+
+// go through the mdn js documentation.
+// also read about the Alert in Js 
+
+let age = 21 ; // number
+let name = "Deepak"; // strings
+let paasedOut  = false ; //boolean
+let location; //undefined
+
+// null - standalone value -> object 
+//symbol - unique
+
+
+console.table(typeof null)
