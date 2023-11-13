f3b3c55c6f655cc85ccb7bff95413c410f98e6f5
imdeepakvermaa <dv24012002@gmail.com>
2023-11-13 14:37:31 +0530

    conversion_Operations

---
+++ Basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let someNumber = 24
+
+// console.log(typeof someNumber)
+
+// let someString = String(someNumber)
+
+// console.log(someString)
+// console.log(typeof someString)
+
+
+// let boolean = Boolean(someNumber)
+// console.log(boolean)
+// console.log(typeof boolean)
+
+
+let string = "Deepak"
+console.log(string);
+
+let num = Number(string) 
+console.log(num)
+console.log(typeof num);
