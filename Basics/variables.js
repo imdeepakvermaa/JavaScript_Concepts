6f7df80e2e567b7af5197fc5e04dc2e34acc56fe
imdeepakvermaa <dv24012002@gmail.com>
2023-11-13 12:43:10 +0530

    Variables

---
+++ Basics/variables.js
@@ -0,0 +1,12 @@
+const name = "Deepak Verma";
+let age = 15;
+var location = "Faridabad";
+college = "niet";
+
+age = 21;
+location = "Bengluru";
+college = "khatam";
+
+console.table([age , location , name , college]);
+
+
