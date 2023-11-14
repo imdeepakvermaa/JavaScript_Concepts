f617cd592222860c76ab07c88e4cb39ee4d8dc9b
imdeepakvermaa <dv24012002@gmail.com>
2023-11-14 11:38:55 +0530

    Summary till now

---
+++ Basics/datatypes_summary.js
@@ -0,0 +1,18 @@
+let num = 1
+let bigNum = 4543213546464556464
+let name = "Deepak Verma"
+let noOfG = null
+let isF = false
+let location;
+let sym1 = Symbol('123')
+let sym2 = Symbol('123')
+
+
+console.log(typeof num);
+console.log(typeof bigNum);
+console.log(typeof name);
+console.log(typeof noOfG);
+console.log(typeof isF);
+console.log(typeof location);
+console.log(typeof sym1);
+console.log(typeof sym2);
