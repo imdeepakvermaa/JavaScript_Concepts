bae95a9a0239c338488467d26d4c3578cdf6349e
imdeepakvermaa <dv24012002@gmail.com>
2023-11-14 20:51:01 +0530

    nums and Math

---
+++ Basics/06_nums_and_maths.js
@@ -0,0 +1,18 @@
+// const score = 400 
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+max = 10
+min = 5
+const num = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(num);
+
+
+
