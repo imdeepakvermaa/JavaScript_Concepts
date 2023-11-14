5ead37566f0f145235d4118f8dbef7e96b5806cb
imdeepakvermaa <dv24012002@gmail.com>
2023-11-14 11:06:06 +0530

    comparison

---
+++ Basics/04_comparison.js
@@ -0,0 +1,31 @@
+let num1 = 14 ;
+// let num2 = 10;
+let num2 = 14;
+
+
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+
+
+
+// let sNum1 = 12;
+// let sNum2 = "Deepak" ;
+// console.log(sNum1 < sNum2);
+// console.log(sNum1 > sNum2);
+// console.log(sNum1 >= sNum2);
+// console.log(sNum1 <= sNum2);
+// console.log(sNum1 === sNum2);
+// console.log(sNum1 == sNum2);
+
+
+
+let sNum1 = "12";
+let sNum2 = "Deepak" ;
+console.log(sNum1 < sNum2);
+console.log(sNum1 > sNum2);
+console.log(sNum1 >= sNum2);
+console.log(sNum1 <= sNum2);
+console.log(sNum1 === sNum2);
+console.log(sNum1 == sNum2);
