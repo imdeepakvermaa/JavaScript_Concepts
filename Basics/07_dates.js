73261c98b86f300d998119d0e0d26da8fcaddb83
imdeepakvermaa <dv24012002@gmail.com>
2023-11-14 22:42:19 +0530

    Dates

---
+++ Basics/07_dates.js
@@ -0,0 +1,30 @@
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+
+// let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());
+
+
+
+
+
+// let myCreatedDate = new Date(2023,0,23,5,33)
+// console.log(myCreatedDate.toLocaleString());
+
+
+
+
+let myCreatedDate = new Date("2002-01-24")
+console.log(myCreatedDate.toLocaleString());
+
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+
+
