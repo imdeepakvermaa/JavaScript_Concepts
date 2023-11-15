99836ae08204f06081f27b33c289419d6294cd76
imdeepakvermaa <dv24012002@gmail.com>
2023-11-15 10:36:31 +0530

    array basics

---
+++ Basics1/01_arrays.js
@@ -0,0 +1,16 @@
+
+
+
+// const arr = ["Deepak",21,"B.Tech.","Computer Science"]
+// console.log(arr);
+
+
+
+// const arr2 = new Array("Deepak",21,"B.Tech.","Computer Science")
+// console.log(arr2);
+
+
+const fruits = [];
+fruits.push("banana", "apple", "peach");  //push
+console.log(fruits.length);   //length
+console.log(fruits);
