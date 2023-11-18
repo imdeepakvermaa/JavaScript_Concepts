5ed3fd7087f2cd5b19c8781b31be8f236851d16e
imdeepakvermaa <dv24012002@gmail.com>
2023-11-18 22:23:10 +0530

    function and object done

---
+++ Basics2/02_functions.js
@@ -0,0 +1,20 @@
+function calculateCartPrice(...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200,4000,20000))
+
+const worldcupinfo = {
+  winningteam: "India",
+  year: 2023,
+  against: "Australia",
+};
+
+function worldCupFinal(anyobject) {
+  console.log(
+    `${anyobject.year} world cup winning team is ${anyobject.winningteam} against ${anyobject.against}`
+  );
+}
+
+worldCupFinal(worldcupinfo)
+
