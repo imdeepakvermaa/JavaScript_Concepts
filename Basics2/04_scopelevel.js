ffddd35e695a20ed45dc6b2bfce825cf95997301
imdeepakvermaa <dv24012002@gmail.com>
2023-11-18 23:06:06 +0530

    scope khatam

---
+++ Basics2/04_scopelevel.js
@@ -0,0 +1,28 @@
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
