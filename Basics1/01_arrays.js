2201d90cba05243f8ba614deefa74695f7f9f2bf
imdeepakvermaa <dv24012002@gmail.com>
2023-11-18 11:31:14 +0530

    Array 2nd part done

--- Basics1/01_arrays.js
+++ Basics1/01_arrays.js
@@ -1,25 +1,25 @@
-// const arr = ["Deepak",21,"B.Tech.","Computer Science"]
-// console.log(arr);
+const arr = ["Deepak",21,"B.Tech.","Computer Science"]
+console.log(arr);
 
-// const arr2 = new Array("Deepak",21,"B.Tech.","Computer Science")
-// console.log(arr2);
+const arr2 = new Array("Deepak",21,"B.Tech.","Computer Science")
+console.log(arr2);
 
-// const fruits = [];
-// fruits.push("banana", "apple", "peach");  //push
-// console.log(fruits.length);   //length
-// console.log(fruits);
+const fruits = [];
+fruits.push("banana", "apple", "peach");  //push
+console.log(fruits.length);   //length
+console.log(fruits);
 
 const array1 = ['a', 'b', 'c'];
-// const array2 = ['d', 'e', 'f'];
-// const array3 = array1.concat(array2);    //concatenate
-// console.log(array3);
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);    //concatenate
+console.log(array3);
 
 console.log(...array1,...array2);
 
 // const array1 = ["a", "b", "c", "d", "e"];
-// console.log(array1.copyWithin(0, 3, 4));
+console.log(array1.copyWithin(0, 3, 4));
 // Copy to index 1 all elements from index 3 to the end
-// console.log(array1.copyWithin(1, 3));
+console.log(array1.copyWithin(1, 3));
 
 const beasts = ["ant", "bison", "camel", "duck", "bison"];
 
---
+++ Basics1/02_arrays.js
@@ -0,0 +1,22 @@
+arr1 = ["deepak", "aniket", "animesh", "aditya"];
+
+// console.log(arr1[2]);
+
+// Array methods
+
+arr1.push("sharma");
+arr1.push("divyanshu");
+
+console.log(arr1);
+arr1.pop();
+
+console.log(arr1);
+
+
+arr1.unshift("divyanshu")
+console.log(arr1);
+
+
+
+arr1.shift()
+console.log(arr1);
