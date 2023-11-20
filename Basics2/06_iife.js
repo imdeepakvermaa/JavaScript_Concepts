413ba2ca6e429944a0538d57fb654ed3a70a32fd
imdeepakvermaa <dv24012002@gmail.com>
2023-11-20 19:29:18 +0530

    ans

--- Basics2/06_iife.js
+++ Basics2/06_iife.js
@@ -1,3 +1,5 @@
 // Immediately Invoked Function Expressions (IIFE)
 
 // what is iife ?
+
+// An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined. 
