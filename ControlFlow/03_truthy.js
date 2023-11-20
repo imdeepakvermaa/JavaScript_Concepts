2ecaa16ef3666ccd653c90dec10f19544df51078
imdeepakvermaa <dv24012002@gmail.com>
2023-11-20 20:18:50 +0530

    null

--- ControlFlow/03_truthy.js
+++ ControlFlow/03_truthy.js
@@ -24,3 +24,12 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
