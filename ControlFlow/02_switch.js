f4dbb0308ae8306fe0f9685cb2740f8dedad98bc
imdeepakvermaa <dv24012002@gmail.com>
2023-11-20 20:15:42 +0530

    switch example

--- ControlFlow/02_switch.js
+++ ControlFlow/02_switch.js
@@ -9,3 +9,22 @@
 
 
 const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
