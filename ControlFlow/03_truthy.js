1c2a9dfc3cd280bd63e6799f979c74c4587cc76a
imdeepakvermaa <dv24012002@gmail.com>
2023-11-20 20:18:26 +0530

    objects

--- ControlFlow/03_truthy.js
+++ ControlFlow/03_truthy.js
@@ -17,3 +17,10 @@ if (userEmail) {
 // if (userEmail.length === 0) {
 //     console.log("Array is empty");
 // }
+
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
