7cdb1ac1dc36bf60182ba62cae392a1161c1af35
imdeepakvermaa <dv24012002@gmail.com>
2023-11-20 19:52:25 +0530

    else

--- ControlFlow/conditional.js
+++ ControlFlow/conditional.js
@@ -28,3 +28,11 @@
 //     console.log("less than 500");
 // } else if (balance < 750) {
 //     console.log("less than 750");
+
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
