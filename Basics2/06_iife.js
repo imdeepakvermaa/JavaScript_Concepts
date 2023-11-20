d3e16cb3a108e31328577f41cb02fd3639f01114
imdeepakvermaa <dv24012002@gmail.com>
2023-11-20 19:29:42 +0530

    named iife

--- Basics2/06_iife.js
+++ Basics2/06_iife.js
@@ -3,3 +3,10 @@
 // what is iife ?
 
 // An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined. 
+
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
