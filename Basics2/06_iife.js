9ca142a0aaf92e3708efc0d2fd82b6d07e11819e
imdeepakvermaa <dv24012002@gmail.com>
2023-11-20 19:30:34 +0530

    db

--- Basics2/06_iife.js
+++ Basics2/06_iife.js
@@ -8,11 +8,11 @@
 
 (function chai(){
     // named IIFE
-    console.log(`DB CONNECTED`);
+    console.log(`DB CONNECTED SUCCESSFULLY`);
 })();
 
 
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('Deepak Verma')
