33f2786366ae3e938fafaed781b2bdae71e9681c
imdeepakvermaa <dv24012002@gmail.com>
2023-11-20 19:20:57 +0530

    code

--- Basics2/05_thisandarrow.js
+++ Basics2/05_thisandarrow.js
@@ -44,3 +44,9 @@ const chai =  () => {
 // const addTwo = (num1, num2) =>  num1 + num2
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
+
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
