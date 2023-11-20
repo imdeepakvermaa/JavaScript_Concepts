a1fe13e877fd699c66b3060f40dedd5e6e2c78d8
imdeepakvermaa <dv24012002@gmail.com>
2023-11-20 19:52:09 +0530

    const

--- ControlFlow/conditional.js
+++ ControlFlow/conditional.js
@@ -11,10 +11,20 @@
 
 // console.log("Execute");
 // <, >, <=, >=, ==, !=, ===, !==
-let score = 450;
-if (score > 100) {
-    let power = "fly"
-    console.log(`User power: ${power}`);
-}
+// let score = 450;
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
 
 // console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
