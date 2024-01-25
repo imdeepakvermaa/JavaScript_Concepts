5c8ee6c00b7e94fc7574c8b692e4052da1cb8011
Deepak Verma <111647177+imdeepakvermaa@users.noreply.github.com>
2024-01-25 13:47:08 +0000

    Hoisting concept in javascript

---
+++ Advanced_Namaste_JS/03_Hoisting.js
@@ -0,0 +1,17 @@
+// var name ;
+// console.log(name);
+// name = "Deepak Verma";
+
+
+// getDetails();
+
+
+function getDetails(name,college){
+    var name = "Deepak Verma";
+    var college = "NiET";
+
+    console.table([name, college]);
+
+};
+
+console.log(getDetails);
