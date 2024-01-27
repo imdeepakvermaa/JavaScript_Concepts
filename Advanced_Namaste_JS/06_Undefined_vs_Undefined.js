3dcbb64b2de3b68b67107862c01d21db3b8f8a88
Deepak Verma <111647177+imdeepakvermaa@users.noreply.github.com>
2024-01-27 08:23:03 +0000

    Undefined and not defiend

---
+++ Advanced_Namaste_JS/06_Undefined_vs_Undefined.js
@@ -0,0 +1,21 @@
+
+
+// undefined
+// console.log(a);
+// var a = 7 ;
+// console.log(a);
+
+
+//not defined
+
+// console.log(a);
+
+
+// var a;
+
+if(a === undefined){
+    console.log("Undefined");
+}
+else{
+    console.log("not defined");
+}
